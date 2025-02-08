2db5aeade34f004b010a8c4491419a29578b9e52
YostinS <yostinsanchezrosario@gmail.com>
2025-02-08 19:39:58 -0400

    Tarea 2

---
+++ Prog1_CSharp_Tarea2_YostinSanchez_20241722_Lunes/Prog1_CSharp_Tarea2_YostinSanchez_20241722_Lunes/Program.cs
@@ -0,0 +1,16 @@
+﻿//Yostin Samuel Sanchez Rosario| Matricula: 2024-1722 | Seccion: Lunes 6pm
+Console.WriteLine("-----------------------");
+Console.WriteLine("Numero par e Impar");
+Console.WriteLine("-----------------------");
+Console.Write("Escriba un numero entero: ");
+int num1 = Console.Read();
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("El numero es par.");
+}
+else
+{
+    Console.WriteLine("El numero es impar.");
+}
+
+Console.ReadKey();
