c2f1d62c8234c6b9ef58e405be0a50c109f566d5
YostinS <yostinsanchezrosario@gmail.com>
2025-03-25 14:04:38 -0400

    Actualizacion 1
    Tarea 4 completa, totalmente funcional

---
+++ Prog1_CSharp_Tarea4_YostinSanchez_20241722_Lunes/Prog1_CSharp_Tarea4_YostinSanchez_20241722_Lunes/Program.cs
@@ -0,0 +1,102 @@
+﻿List<decimal> typedNumbers = new List<decimal>();
+decimal result = 0;
+int typedOption = 0;
+bool running  = true;
+
+//Si se pregunta porque es diferente al codigo que usted proporciono es que lo hice desde 0 en base a la proporcionada
+
+while (running)
+{
+    try
+    {
+        Display();
+        typedOption = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"You need to choose a correct option: {ex.Message}");
+    }
+    if (typedOption == 5)
+    { 
+        running = false;
+        Console.WriteLine("Closing program...");
+    }
+    else
+    {
+        try
+        {
+            Console.WriteLine("Please type the first number");
+            typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine("Please type the second number");
+            typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"You need to choose a correct option: {ex.Message}");
+        }
+        switch (typedOption)
+        {
+            case 1:
+                {
+                    Sum(ref result, typedNumbers);
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                }
+            case 2:
+                {
+                    Sustrac(ref result, typedNumbers);
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                }
+            case 3:
+                {
+                    Multiplication(ref result, typedNumbers);
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                }
+            case 4:
+                {
+                    Divition(ref result, typedNumbers);
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("ERROR");
+                    break;
+                }
+        }
+
+        typedNumbers.Clear();
+    }
+}
+static void Display()
+{
+
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("Please type the option number.\n 1- Sum.\n 2- Sustrac.\n 3- Multiplication. \n 4- Divition. \n 5- Exit.");
+    Console.WriteLine("--------------------------------");
+}
+static void Sum(ref decimal result, List<decimal> typedNumbers)
+{
+    result = typedNumbers[0] + typedNumbers[1];
+}
+static void Sustrac(ref decimal result, List<decimal> typedNumbers)
+{
+    result = typedNumbers[0] - typedNumbers[1];
+}
+static void Multiplication(ref decimal result, List<decimal> typedNumbers) 
+{ 
+    result = typedNumbers[0] * typedNumbers[1];
+}
+static void Divition(ref decimal result, List<decimal> typedNumbers)
+{
+    try
+    {
+        result = typedNumbers[0] / typedNumbers[1];
+    }
+    catch(DivideByZeroException ex) {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
